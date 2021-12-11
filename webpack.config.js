96f5e96349faf1bda6d907f7fbcbc1fd89cf1234
Szecsődi Imre <imre.secodi@gmail.com>
2021-12-11 19:18:58 +0100

    add files

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const WebpackBar = require('webpackbar');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    devtool: "source-map",
+    entry: {
+        "main": "./src/client/main.tsx"
+    },
+    devServer: {
+        contentBase: "./",
+        publicPath: "/dist",
+        hot: true
+    },
+    output: {
+        filename: "[name].js",
+        libraryTarget: "umd",
+        globalObject: "this",
+        path: path.resolve(__dirname, 'out/dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: [/node_modules/],
+                loader: "ts-loader",
+                options: {
+                    configFile: path.resolve(__dirname, "tsconfig.json")
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.svg$/,
+                use: ['preact-svg-loader'],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+        "alias": {
+            "react": "preact/compat",
+            "react-dom/test-utils": "preact/test-utils",
+            "react-dom": "preact/compat",
+            // Must be below test-utils
+        },
+    },
+    plugins: [
+        new WebpackBar(),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "client", "index.html"),
+        }),
+    ]
+}
