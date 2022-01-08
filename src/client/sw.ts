4db316f9d0097e06079e8153eb49aaa964250aa0
Szecsődi Imre <imre.secodi@gmail.com>
2022-01-08 20:11:16 +0100

    repalce node-sass with sass

--- src/client/sw.ts
+++ src/client/sw.ts
@@ -3,22 +3,3 @@ declare var self: ServiceWorkerGlobalScope;
 export {};
 
 console.log("[Service worker] It just works")
-self.addEventListener("install", (event: ExtendableEvent) => {
-    event.waitUntil(
-        caches.open("v1").then(cache => {
-            return cache.addAll([
-                "/",
-                "/index.html",
-            ])
-        })
-    )
-})
-
-self.addEventListener('activate', (event: ExtendableEvent)  => {
-    
-})
-
-self.addEventListener("fetch", (event: FetchEvent) => {
-
-})
-
